539b5a2efdb6ae8821a5603ebc62b9704652cb89
Camilo Schaser-Hughes <camilo.schaserhughes@gmail.com>
2022-07-12 00:14:18 -0700

    moved PhoneCallChecker to it's own class, so can work in parser maybe.

--- phonebill/src/main/java/edu/pdx/cs410J/camilo3/Project2.java
+++ phonebill/src/main/java/edu/pdx/cs410J/camilo3/Project2.java
@@ -64,52 +64,6 @@ public class Project2 {
   }
 
   /**
-   * returns true if is of the form nnn-nnn-nnnn, false otherwise
-   */
-  @VisibleForTesting
-  static boolean isValidPhoneNumber(String phoneNumber) {
-    return Pattern.matches("^\\d{3}-\\d{3}-\\d{4}$", phoneNumber);
-  }
-
-  /**
-   * returns true if date is of the regex form ^\\d?\\d/\\d?\\d/\\d{4}$
-   * false otherwise
-   */
-  @VisibleForTesting
-  static boolean isValidDate(String date) {
-    return Pattern.matches("^\\d?\\d/\\d?\\d/\\d{4}$", date);
-  }
-
-  /**
-   * returns true if time is of the regex form ^[012]?\\d:[0-5]\\d$
-   * false otherwise
-   */
-  @VisibleForTesting
-  static boolean isValidTime(String time) {
-    return Pattern.matches("^[012]?\\d:[0-5]\\d$", time);
-  }
-
-
-  /**
-   * takes the args and makes sure they are the correct length, and then
-   * makes sure each individual argument is in the correct format,
-   * responds with an exception depending on which argument is invalid.
-   */
-  static void checkForImproperFormatting(String[] args)
-          throws MissingCommandLineArguments, ImproperPhoneNumber, ImproperDate, ImproperTime {
-    if (args.length != 7) {
-      throw new MissingCommandLineArguments();
-    } else if (! isValidPhoneNumber(args[1]) || ! isValidPhoneNumber(args[2])) {
-      throw new ImproperPhoneNumber();
-    } else if (! isValidDate(args[3]) || ! isValidDate(args[5])) {
-      throw new ImproperDate();
-    } else if (! isValidTime(args[4]) || ! isValidTime(args[6])) {
-      throw new ImproperTime();
-    }
-  }
-
-
-  /**
    * main method, can take is arguments from the command line, parses and checks them for
    * validity and prints exception messages if there are any
    * makes a {@link PhoneCall} object and a {@link PhoneBill} and adds phoneCall to
@@ -141,7 +95,8 @@ public class Project2 {
       // this part checks for the formatting.
       // and then creates the actual objects with the formatted array.
       try {
-        checkForImproperFormatting(clippedArgs);
+        PhoneCallChecker checker = new PhoneCallChecker();
+        checker.checkForImproperFormatting(clippedArgs);
         aBill = new PhoneBill(clippedArgs[0]);
         aCall = new PhoneCall(
                 clippedArgs[1], clippedArgs[2], clippedArgs[3] + " " + clippedArgs[4],
@@ -156,51 +111,4 @@ public class Project2 {
     }
   } // end of main
 
-  /**
-   * exception that is thrown when the phone number arguments are messed up.
-   */
-  static class ImproperPhoneNumber extends Exception {
-    public ImproperPhoneNumber() {
-      super( "INCORRECT FORMATTING OF PHONE NUMBERS\n" +
-              "should be nnn-nnn-nnnn where n is a digit btwn 0-9\n" +
-              "seperated by hyphens.\n" +
-              "example: 503-867-5309\n" + "Thank you.");
-    }
-  }
-
-  /**
-   * exception that is thrown when the date arguments are messed up.
-   */
-  static class ImproperDate extends Exception {
-    public ImproperDate() {
-      super("INCORRECT FORMATTING OF DATES\n" +
-              "should be nn/nn/nnnn or n/n/nnnn where n is a\n" +
-              "digit btwn 0-9 seperated by backslashes.\n" +
-              "example: 03/7/2022\n" + "Thank you.");
-    }
-  }
-
-  /**
-   * exception that is thrown when the time arguments are messed up.
-   */
-  static class ImproperTime extends Exception {
-    public ImproperTime() {
-      super("INCORRECT FORMATTING OF TIMES\n" +
-              "should be nn:nn or n:nn where n is a\n" +
-              "digit btwn 0-9 seperated by a colon.\n" +
-              "example: 03:42 or 11:13\n" + "Thank you.");
-    }
-  }
-
-  /**
-   * exception that is thrown when the time arguments are messed up.
-   */
-  @VisibleForTesting
-  static class MissingCommandLineArguments extends Exception {
-    public MissingCommandLineArguments() {
-      super( "INCORRECT USE OF COMMAND LINE ARGUMENTS\n" +
-              "usage: java -jar target/phonebill-2022.0.0.jar [options] <args>\n" +
-              "Please use option -README for complete list of options and args.");
-    }
-  }
 }
---
+++ phonebill/src/test/java/edu/pdx/cs410J/camilo3/PhoneCallCheckerTest.java
@@ -0,0 +1,148 @@
+package edu.pdx.cs410J.camilo3;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * A unit test for code in the <code>PhoneCallChecker</code> class.
+ */
+
+public class PhoneCallCheckerTest {
+
+    /**
+     * test the two paths for isValidPhoneNumber() function.
+     */
+    @Test
+    void isValidPhoneNumberTrue() {
+        PhoneCallChecker checker = new PhoneCallChecker();
+
+        assertEquals(checker.isValidPhoneNumber("503-867-5309"), true);
+    }
+
+    @Test
+    void isValidPhoneNumberFalse() {
+        PhoneCallChecker checker = new PhoneCallChecker();
+
+        assertEquals(checker.isValidPhoneNumber("50t-867-5309"), false);
+    }
+
+    /**
+     * test the two paths for isValidDate() function.
+     */
+    @Test
+    void isValidDateTrue() {
+        PhoneCallChecker checker = new PhoneCallChecker();
+
+        assertEquals(checker.isValidDate("3/15/2022"), true);
+        assertEquals(checker.isValidDate("03/2/2022"), true);
+    }
+
+    @Test
+    void isValidDateFalse() {
+        PhoneCallChecker checker = new PhoneCallChecker();
+
+        assertEquals(checker.isValidDate("003/02/1901"), false);
+        assertEquals(checker.isValidDate("3/2/901"), false);
+    }
+
+    /**
+     * test the two paths for isValidTime() function.
+     */
+    @Test
+    void isValidTimeTrue() {
+        PhoneCallChecker checker = new PhoneCallChecker();
+
+        assertEquals(checker.isValidTime("11:13"), true);
+        assertEquals(checker.isValidTime("09:59"), true);
+    }
+
+    @Test
+    void isValidTimeFalse() {
+        PhoneCallChecker checker = new PhoneCallChecker();
+
+        assertEquals(checker.isValidTime("003/02/1901"), false);
+        assertEquals(checker.isValidTime("45:99"), false);
+    }
+
+    /**
+     *  These are just all the ways that formatting can fail!!!
+     */
+    @Test
+    void checkFormattingFailsOnZero() {
+        PhoneCallChecker checker = new PhoneCallChecker();
+        String[] testArgs = new String[0];
+        Exception exception = assertThrows(PhoneCallChecker.MissingCommandLineArguments.class, () -> {
+            checker.checkForImproperFormatting(testArgs);
+        });
+        assertTrue(exception.getMessage().contains("INCORRECT USE OF COMMAND LINE ARGUMENTS"));
+    }
+
+    @Test
+    void checkFormattingFailsOnBadFirstPhoneNumber() {
+        PhoneCallChecker checker = new PhoneCallChecker();
+        String[] testArgs = new String[] {"dave", "8k1-227-1838", "831-479-4859", "date1", "time1", "date2", "time2"};
+        Exception exception = assertThrows(PhoneCallChecker.ImproperPhoneNumber.class, () -> {
+            checker.checkForImproperFormatting(testArgs);
+        });
+        assertTrue(exception.getMessage().contains("INCORRECT FORMATTING OF PHONE NUMBERS"));
+    }
+
+    @Test
+    void checkFormattingFailsOnBadSecondPhoneNumber() {
+        PhoneCallChecker checker = new PhoneCallChecker();
+        String[] testArgs = new String[] {"dave", "831-479-4859", "8k1-227-1838", "date1", "time1", "date2", "time2"};
+        Exception exception = assertThrows(PhoneCallChecker.ImproperPhoneNumber.class, () -> {
+            checker.checkForImproperFormatting(testArgs);
+        });
+        assertTrue(exception.getMessage().contains("INCORRECT FORMATTING OF PHONE NUMBERS"));
+    }
+
+    @Test
+    void checkFormattingFailsOnBadFirstDate() {
+        PhoneCallChecker checker = new PhoneCallChecker();
+        String[] testArgs = new String[] {"dave", "831-479-4859", "861-227-1838", "003/7/2022", "time1", "03/7/2022", "time2"};
+        Exception exception = assertThrows(PhoneCallChecker.ImproperDate.class, () -> {
+            checker.checkForImproperFormatting(testArgs);
+        });
+        assertTrue(exception.getMessage().contains("INCORRECT FORMATTING OF DATES"));
+    }
+
+    @Test
+    void checkFormattingFailsOnBadSecondDate() {
+        PhoneCallChecker checker = new PhoneCallChecker();
+        String[] testArgs = new String[] {"dave", "831-479-4859", "861-227-1838", "03/7/2022", "time1", "03/007/2022", "time2"};
+        Exception exception = assertThrows(PhoneCallChecker.ImproperDate.class, () -> {
+            checker.checkForImproperFormatting(testArgs);
+        });
+        assertTrue(exception.getMessage().contains("INCORRECT FORMATTING OF DATES"));
+    }
+
+    @Test
+    void checkFormattingFailsOnBadFirstTime() {
+        PhoneCallChecker checker = new PhoneCallChecker();
+        String[] testArgs = new String[] {"dave", "831-479-4859", "861-227-1838", "03/7/2022", "33:42", "03/7/2022", "01:47"};
+        Exception exception = assertThrows(PhoneCallChecker.ImproperTime.class, () -> {
+            checker.checkForImproperFormatting(testArgs);
+        });
+        assertTrue(exception.getMessage().contains("INCORRECT FORMATTING OF TIMES"));
+    }
+
+    @Test
+    void checkFormattingFailsOnBadSecondTime() {
+        PhoneCallChecker checker = new PhoneCallChecker();
+        String[] testArgs = new String[] {"dave", "831-479-4859", "861-227-1838", "03/7/2022", "01:42", "03/07/2022", "001:47"};
+        Exception exception = assertThrows(PhoneCallChecker.ImproperTime.class, () -> {
+            checker.checkForImproperFormatting(testArgs);
+        });
+        assertTrue(exception.getMessage().contains("INCORRECT FORMATTING OF TIMES"));
+    }
+}
