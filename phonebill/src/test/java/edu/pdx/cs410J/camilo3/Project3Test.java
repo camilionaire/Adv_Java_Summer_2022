e167094106fea954e8315cb5d53fe011cd8b8d09
Camilo Schaser-Hughes <camilo.schaserhughes@gmail.com>
2022-07-19 19:05:17 -0700

    clean up Proj3Test

--- phonebill/src/test/java/edu/pdx/cs410J/camilo3/Project3Test.java
+++ phonebill/src/test/java/edu/pdx/cs410J/camilo3/Project3Test.java
@@ -4,16 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 /**
  * A unit test for code in the <code>Project1</code> class.  This is different
  * from <code>Project1IT</code> which is an integration test (and can capture data
@@ -22,18 +12,24 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 class Project3Test {
 
-  // ALSO GOING TO ASK IN OFFICE HOURS ABOUT THIS, WHICH IS WHY IS HERE ON TURNIN.
-//  @Test
-//  void testEverythingWorksWonderfully() throws Exception {
-//      Project2 proj = new Project2();
-//      String[] args = new String[] {"Steven", "867-867-5309", "503-222-2222",
-//                    "03/17/2022", "23:11", "03/17/2022", "23:27"};
-//      proj.main(args);
-//
-//  }
-
+    /**
+     * Just runs the current standard assumed command line inputs into the
+     * project 3 and makes sure no bells or whistles sound off
+     */
   @Test
-  void checkNamesMatchErrorThrown() throws Project3.NamesDontMatch {
+  void testEverythingWorksWonderfully() {
+      Project3 proj = new Project3();
+      String[] args = new String[] {"Steven", "867-867-5309", "503-222-2222",
+                    "03/17/2022", "11:11", "am", "03/17/2022", "12:01", "pm"};
+      proj.main(args);
+
+  }
+
+    /**
+     * one error still handled within project 3.
+     */
+  @Test
+  void checkNamesMatchErrorThrown() {
     Project3 proj = new Project3();
     Exception exception = assertThrows(Project3.NamesDontMatch.class, () -> {
       proj.checkNamesMatch("hello", "goodbye");
