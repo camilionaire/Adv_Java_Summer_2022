963eed9e6cafe9e8f021fba3f3d3a776e48a057b
Camilo Schaser-Hughes <camilo.schaserhughes@gmail.com>
2022-08-03 15:38:55 -0700

    added a readme page.

--- phonebill-android/app/src/main/java/edu/pdx/cs410j/camilo3/MainActivity.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/camilo3/MainActivity.java
@@ -2,12 +2,16 @@ package edu.pdx.cs410j.camilo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // from the tutorial if I want to sent a message.
+//    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +21,15 @@ public class MainActivity extends AppCompatActivity {
     public void sayHello(View view) {
         PhoneCall call = new PhoneCall();
         Toast.makeText(this, "The call is:" + call, Toast.LENGTH_SHORT).show();
+    }
+
+    /** this is called when the readme button is pressed. */
+    public void goToReadMe(View view) {
+        Intent intent = new Intent(this, DisplayReadMe.class);
+        // from the tutorial if I want to send a message.
+//        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
