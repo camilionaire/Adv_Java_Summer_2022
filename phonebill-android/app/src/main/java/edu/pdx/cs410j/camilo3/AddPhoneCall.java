a68022597d8cb3f26d8f59f375d3b13a4f6d89b6
Camilo Schaser-Hughes <camilo.schaserhughes@gmail.com>
2022-08-06 00:23:37 -0700

    add reads in customer name on submit.

--- phonebill-android/app/src/main/java/edu/pdx/cs410j/camilo3/AddPhoneCall.java
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/camilo3/AddPhoneCall.java
@@ -3,6 +3,9 @@ package edu.pdx.cs410j.camilo3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddPhoneCall extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class AddPhoneCall extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_phone_call);
+    }
+
+    public void addPhoneCall(View view) {
+        EditText cust = findViewById(R.id.addCustomerName);
+//        PhoneCall aCall = new PhoneCall();
+
+        Toast.makeText(this, "NAME WAS: " + cust.getText(), Toast.LENGTH_LONG).show();
     }
 }
