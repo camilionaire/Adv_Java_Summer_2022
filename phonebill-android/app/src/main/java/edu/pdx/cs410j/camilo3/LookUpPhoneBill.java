d58539a2fde9ec6a16676807225df380369a0d4d
Camilo Schaser-Hughes <camilo.schaserhughes@gmail.com>
2022-08-06 01:38:12 -0700

    added look up phon bill and search phone bill activities.

---
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/camilo3/LookUpPhoneBill.java
@@ -0,0 +1,14 @@
+package edu.pdx.cs410j.camilo3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LookUpPhoneBill extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_look_up_phone_bill);
+    }
+}
---
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/camilo3/SearchPhoneCalls.java
@@ -0,0 +1,14 @@
+package edu.pdx.cs410j.camilo3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchPhoneCalls extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_phone_calls);
+    }
+}
