77623c4f09c733ca9458428b9a047b3f2b8a0969
Camilo Schaser-Hughes <camilo.schaserhughes@gmail.com>
2022-08-10 11:34:40 -0700

    added activity for displaying phone bills...

---
+++ phonebill-android/app/src/main/java/edu/pdx/cs410j/camilo3/DisplayPhoneBill.java
@@ -0,0 +1,14 @@
+package edu.pdx.cs410j.camilo3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayPhoneBill extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_phone_bill);
+    }
+}
